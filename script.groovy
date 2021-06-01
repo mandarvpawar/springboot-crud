d4f7b36cc9bce802a2e29fb379ed6e3bffcaee1c
Mandar Pawar <mandarvpawar@gmail.com>
2021-06-01 16:43:02 +0530

    Modified script file

--- script.groovy
+++ script.groovy
@@ -1,7 +1,23 @@
+def map = [
+        Bob  : 42,
+        Alice: 54,
+        Max  : 33
+]
+
 def buildApp() {
-  echo 'Building the application'
-  echo "Version: ${Version}"
-  echo "Version: ${env.Version}"
+  	echo 'Building the application'
+  	echo "Version: ${Version}"
+  	echo "Version: ${env.Version}"
+  
+	script {
+		map.each { entry ->
+	    	stage (entry.key) {
+	        	timestamps{
+	            	echo "$entry.value"
+	        	}
+	    	}
+		}
+	}
 }
 
 def testApp() {
